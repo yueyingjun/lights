a793206755a14ab4837fd6de2fd70b66a388b2bb
yueyingjuna <1046396106@qq.com>
2017-12-22 11:48:52 +0800

    a

--- app.js
+++ app.js
@@ -1,43 +1,21 @@
 var light=require("./light");
 var child=require("child_process");
 var app=light();
-/*对客户端信息的接收能力  响应能力*/
 app.listen(9999,function(){
     console.log(app.port,"start11");
 });
 
-
-/*
-*
-  express
-
-  "/list/1"
-
-  /^\/list\/[^\/]+/
-  router.get("/list/:id",function(){
-  })
-* */
-
-var url="/list/zhangsan";
-
-var mode="/list/:id";
-
-
-
-var str=mode.replace(/:[^\/]+/,"([^\/]+)");
-
-str=str.replace(/\//g,"\\/")
-str="/^"+str+"/";
-console.log(eval(str).exec(url)[1]);
-
-
-
-app.get("/abc",function(req,res){
-   res.send("abc")
+app.get("/",function(req,res){
+   res.sendFile("abc/1.html")
 })
 
 app.get("/list/:id",function(req,res){
     res.send(req.params.id);
+})
+app.get("/form",function(req,res){
+    var datas="";
+     console.log(req.url);
+    res.end("ok");
 })
 
 
--- light.js
+++ light.js
@@ -1,131 +1,147 @@
-var http=require("http");
-var config=require("./config.js");
-var path=require("path")
-var fs=require("fs");
-class light{
-  constructor(){
-      this.getInfo=[];
-      this.postInfo=[];
-      this.rootUrl=process.cwd();
-  }
+var http = require("http");
+var config = require("./config.js");
+var path = require("path")
+var fs = require("fs");
+class light {
+    constructor() {
+        this.getInfo = [];
+        this.postInfo = [];
+        this.rootUrl = process.cwd();
+    }
 
-  listen(port,fn){
-      if(arguments.length==0){
-          var port=config.port;
-          var fn=function(){
-              console.log(port)
-          }
-      }else if(arguments.length==1){
-          if(typeof port=="number"){
-              var port=port;
-              var fn=function(){
-                  console.log(port)
-              }
+    listen(port, fn) {
+        if (arguments.length == 0) {
+            var port = config.port;
+            var fn = function () {
+                console.log(port)
+            }
+        } else if (arguments.length == 1) {
+            if (typeof port == "number") {
+                var port = port;
+                var fn = function () {
+                    console.log(port)
+                }
 
-          }else if(typeof port=="function"){
-              var fn=port;
-              var port=config.port;
+            } else if (typeof port == "function") {
+                var fn = port;
+                var port = config.port;
 
-          }else{
-              var port=config.port;
-              var fn=function(){
-                  console.log(port)
-              }
+            } else {
+                var port = config.port;
+                var fn = function () {
+                    console.log(port)
+                }
 
-          }
-      }else if(arguments.length==2){
-            var port=port;
-            var fn=fn;
-      }
+            }
+        } else if (arguments.length == 2) {
+            var port = port;
+            var fn = fn;
+        }
 
-      this.port=port;
+        this.port = port;
 
-      http.createServer((req,res)=>{
-         var methods=(req.method);
-                this.run(req.method,req,res)
+        http.createServer((req, res) => {
+            var methods = (req.method);
+            this.run(req.method, req, res)
 
-      }).listen(port,function(){
-          if(fn){
-              fn();
-          }
-      })
-  }
+        }).listen(port, function () {
+            if (fn) {
+                fn();
+            }
+        })
+    }
 
-  run(type,req,res){
-    var url=req.url;
-    this.extend(req,res);
-   if(url=="/favicon.ico"){
-       res.end();
-   }else {
-       if(type=="GET"){
+    run(type, req, res) {
+        var url = req.url;
+        this.extend(req, res);
+        if (url == "/favicon.ico") {
+            res.end();
+        } else {
+            this.request(req,res,type,url)
+        }
+    }
 
-             var flag=true;
-             for(var i=0;i<this.getInfo.length;i++){
-                 var reg=eval(this.getInfo[i].url);
-                 if(reg.test(url)){
-                     flag=false
-                     req.params={};
-                     var result=reg.exec(url);
-                     for(var j=0;j<result.length;j++){
-                         req.params[this.getInfo[i].attr[j]]=result[j+1];
+    request(req,res,type,url){
+        if(type=="GET"){
+            var arr=this.getInfo;
+        }else{
+            var arr=this.postInfo;
+        }
+        var flag = true;
+        for (var i = 0; i < arr.length; i++) {
+            var reg = eval(arr[i].url);
+            if (reg.test(url)) {
+                flag = false
+                req.params = {};
+                var result = reg.exec(url);
+                for (var j = 0; j < result.length; j++) {
+                    req.params[arr[i].attr[j]] = result[j + 1];
 
-                     }
-                     this.getInfo[i].callback(req,res);
-                     break;
-                 }
+                }
+                arr[i].callback(req, res);
+                break;
+            }
 
-             }
-             if(flag){
-                 res.end("err");
-             }
+        }
+        if (flag) {
+            res.end("err");
+        }
 
+    }
 
+    get(url, fn) {
+        this.saveInfo(url,fn,"get")
+    }
+    post(url, fn) {
+        this.saveInfo(url,fn,"post")
+    }
+    all(url,fn){
+        this.saveInfo(url,fn,"get")
+        this.saveInfo(url,fn,"post")
+    }
 
-       }else{
+    saveInfo(url,fn,type){
 
-
-       }
-
-   }
-  }
-
-  get(url,fn){
+        var infoArr=type=="get"?this.getInfo:this.postInfo;
         //路由的匹配
-      var arr=url.match(/:([^\/]+)/g)||[];
-      arr=arr.map(function (item) {
-          return item.substr(1);
-      });
+        var arr = url.match(/:([^\/]+)/g) || [];
+        arr = arr.map(function (item) {
+            return item.substr(1);
+        });
+        var str = url.replace(/:[^\/]+/g, "([^\/]+)");
+        str = str.replace(/\//g, '\\/');
+        str = "/^" + (str) + '[\\/]?(?:\\?.*)?$/';
+        var obj = {};
+        obj["url"] = str;
+        obj.callback = fn;
+        obj.attr = arr;
+        infoArr.push(obj);
+    }
 
-      var str=url.replace(/:[^\/]+/g,"([^\/]+)");
-      str=str.replace(/\//g,'\\/');
-      str="/^"+(str)+'[\\/]?$/';
 
-       var obj={};
-       obj["url"]=str;
-       obj.callback=fn;
-       obj.attr=arr;
-       this.getInfo.push(obj);
-  }
 
-  extend(req,res){
-      res.send=function(message){
-          res.end(message);
-      };
-      res.sendFile=(url)=>{
-          var fullpath=path.join(this.rootUrl,url);
-          fs.stat(fullpath,function(err){
-              if(err){
-                  res.end(err.toString())
-              }else{
-                  fs.createReadStream(fullpath).pipe(res);
-              }
-          })
+    extend(req, res) {
+        res.send = function (message) {
+            res.end(message);
+        };
+        res.sendFile = (url) => {
+            var fullpath = path.join(this.rootUrl, url);
+            fs.stat(fullpath, function (err) {
+                if (err) {
+                    res.end(err.toString())
+                } else {
+                    fs.createReadStream(fullpath).pipe(res);
+                }
+            })
 
-      }
-  }
+        }
+
+
+    }
+
 
 }
 
-module.exports=function(){
-   return new light();
+module.exports = function () {
+    return new light();
 }
