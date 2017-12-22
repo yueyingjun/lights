b7945cd1ae88ad3f574634d66ea30ffb3f70488b
yueyingjuna <1046396106@qq.com>
2017-12-22 10:24:39 +0800

    demo

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 var light=require("./light");
+var child=require("child_process");
 var app=light();
 /*对客户端信息的接收能力  响应能力*/
 app.listen(9999,function(){
-    console.log(app.port,"start")
+    console.log(app.port,"start11");
 });
 
 
+/*
+*
+  express
 
-app.get("/",function(req,res){
-res.end("/")
-})
+  "/list/1"
+
+  /^\/list\/[^\/]+/
+  router.get("/list/:id",function(){
+  })
+* */
+
+var url="/list/zhangsan";
+
+var mode="/list/:id";
+
+
+
+var str=mode.replace(/:[^\/]+/,"([^\/]+)");
+
+str=str.replace(/\//g,"\\/")
+str="/^"+str+"/";
+console.log(eval(str).exec(url)[1]);
+
+
+
 app.get("/abc",function(req,res){
-res.end("abc");
+   res.send("abc")
 })
-app.get("/aaa",function(req,res){
-res.end("aaa");
+
+app.get("/list/:id",function(req,res){
+    res.send(req.params.id);
 })
+
 
--- light.js
+++ light.js
@@ -1,12 +1,15 @@
 var http=require("http");
 var config=require("./config.js");
+var path=require("path")
+var fs=require("fs");
 class light{
-
   constructor(){
-      this.routeInfo={};
+      this.getInfo=[];
+      this.postInfo=[];
+      this.rootUrl=process.cwd();
   }
-  listen(port,fn){
 
+  listen(port,fn){
       if(arguments.length==0){
           var port=config.port;
           var fn=function(){
@@ -39,11 +42,7 @@ class light{
 
       http.createServer((req,res)=>{
          var methods=(req.method);
-         if(methods=="GET"){
-                this.run(req,res)
-         }
-
-          res.end();
+                this.run(req.method,req,res)
 
       }).listen(port,function(){
           if(fn){
@@ -52,22 +51,77 @@ class light{
       })
   }
 
-  run(req,res){
+  run(type,req,res){
     var url=req.url;
+    this.extend(req,res);
    if(url=="/favicon.ico"){
        res.end();
    }else {
-       var fn = this.routeInfo[url];
-       if (fn) {
-           fn(req, res);
-       } else {
-           res.end("err");
+       if(type=="GET"){
+
+             var flag=true;
+             for(var i=0;i<this.getInfo.length;i++){
+                 var reg=eval(this.getInfo[i].url);
+                 if(reg.test(url)){
+                     flag=false
+                     req.params={};
+                     var result=reg.exec(url);
+                     for(var j=0;j<result.length;j++){
+                         req.params[this.getInfo[i].attr[j]]=result[j+1];
+
+                     }
+                     this.getInfo[i].callback(req,res);
+                     break;
+                 }
+
+             }
+             if(flag){
+                 res.end("err");
+             }
+
+
+
+       }else{
+
+
        }
+
    }
   }
 
   get(url,fn){
-        this.routeInfo[url]=fn;
+        //路由的匹配
+      var arr=url.match(/:([^\/]+)/g)||[];
+      arr=arr.map(function (item) {
+          return item.substr(1);
+      });
+
+      var str=url.replace(/:[^\/]+/g,"([^\/]+)");
+      str=str.replace(/\//g,'\\/');
+      str="/^"+(str)+'[\\/]?$/';
+
+       var obj={};
+       obj["url"]=str;
+       obj.callback=fn;
+       obj.attr=arr;
+       this.getInfo.push(obj);
+  }
+
+  extend(req,res){
+      res.send=function(message){
+          res.end(message);
+      };
+      res.sendFile=(url)=>{
+          var fullpath=path.join(this.rootUrl,url);
+          fs.stat(fullpath,function(err){
+              if(err){
+                  res.end(err.toString())
+              }else{
+                  fs.createReadStream(fullpath).pipe(res);
+              }
+          })
+
+      }
   }
 
 }
